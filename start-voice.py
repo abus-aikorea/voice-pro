18d44792eb166c7aa91c4c3f4d8087df3feca890
abus.aikorea <abus.aikorea@gmail.com>
2024-11-04 17:11:55 +0900

    Whisper engine selectable(openai-whisper, faster-whisper, whisper-timestamped). Support whisper turbo models.

--- start-voice.py
+++ start-voice.py
@@ -8,9 +8,9 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
 
-from src.config import ApplicationConfig, UserConfig
+from src.config import UserConfig
 from src.hf_models import hf_download_models
-from app.abus_genuine import genuine_init, genuine_is_paid
+from app.abus_genuine import genuine_init
 from app.abus_app_voice import create_ui
 from app.abus_path import path_workspace_folder, path_gradio_folder
 
@@ -22,11 +22,8 @@ hf_download_models(file_type='demucs', level=0)
 path_workspace_folder()
 path_gradio_folder()
 
-config_filename = "config" + ("-paid" if genuine_is_paid() else "-free") + ".json5"
-config_path = os.path.join(Path(__file__).resolve().parent, "app", config_filename)
-default_app_config = ApplicationConfig.parse_file(config_path)       
 
 user_config_path = os.path.join(Path(__file__).resolve().parent, "app", "config-user.json5")
 user_config = UserConfig(user_config_path)
 
-create_ui(app_config=default_app_config, user_config=user_config)
+create_ui(user_config=user_config)
